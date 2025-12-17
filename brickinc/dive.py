a0860de9d95a5557a4d36cdb3d80de24d0c2e881
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2025-12-17 08:28:03 +0100

    Ending dive when dive needs to end

--- brickinc/dive.py
+++ brickinc/dive.py
@@ -7,11 +7,17 @@ from utils.ochestrator import *
 from brickinc.upgrades import *
 from brickinc.brick import *
 from brickinc.rank import *
+from datetime import datetime, timedelta
+
+DIVE_DURATION = 5 * 60 # seconds
+ALMOST_DONE = 20 # seconds
 
 def dive():
-    rankUp = RankUp.SMALL_RANK_UP
+    rankUp = RankUp.SMALL_RANK_UP 
+    startTime = datetime.now()
+    endTime = startTime + timedelta(seconds=DIVE_DURATION)
 
-    while True:
+    while datetime.now() < endTime:
         
         if rankUp != RankUp.NO_RANK_UP:
             upgradeFields()
@@ -26,7 +32,13 @@ def dive():
             upgradeFields()
             upgradeAllUpgrades()
 
+            if datetime.now() >= endTime - timedelta(seconds=ALMOST_DONE):
+                print("Almost done with dive, exiting loop early to rank up before end.")
+                break
+
         rankUp = upgradeRank()
+    
+    print("Dive complete.")
 
 
 dive()
